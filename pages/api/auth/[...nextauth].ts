4aae11d27f593e3e6c3e19285bf585e28f496b04
Jad Slim <jadslimm@gmail.com>
2023-12-27 17:29:07 -0500

    checkpoint

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth";
 import TwitterProvider from "next-auth/providers/twitter";
-// import { TwitterProfile } from "next-auth/providers/twitter";
+import { TwitterProfile } from "next-auth/providers/twitter";
 import { NextApiRequest, NextApiResponse } from "next";
-// import {ExtendedUser, ExtendedJWT} from '../../../types/types'
+import {ExtendedUser, ExtendedJWT} from '../../../types/types'
 
 
 export default async function auth(
@@ -17,48 +17,48 @@ export default async function auth(
         clientSecret: process.env.TWITTER_CONSUMER_SECRET || "",
         version: "2.0",
         
-        // userinfo: {
-        //   url: "https://api.twitter.com/2/users/me",
-        //   params: {
-        //     "user.fields": "url,entities",
-        //   },
-        // },
-        // profile(profile: { data: TwitterProfile["data"] }) {
-        //   return {
-        //     id: profile.data.id,
-        //     name: profile.data.name,
-        //     username: profile.data.username,
-        //   };
-        // },
+        userinfo: {
+          url: "https://api.twitter.com/2/users/me",
+          params: {
+            "user.fields": "url,entities",
+          },
+        },
+        profile(profile: { data: TwitterProfile["data"] }) {
+          return {
+            id: profile.data.id,
+            name: profile.data.name,
+            username: profile.data.username,
+          };
+        },
       }),
     ],
-    // callbacks: {
-    //   async jwt({ token, user }) {
-    //     if (user) {
-    //       const extendedUser = user as ExtendedUser;
+    callbacks: {
+      async jwt({ token, user }) {
+        if (user) {
+          const extendedUser = user as ExtendedUser;
 
-    //       token.user = {
-    //         id: extendedUser.id.toString(),
-    //         username: extendedUser.username,
-    //         name: extendedUser.name,
-    //       };
-    //     }
-    //     return token as ExtendedJWT;
-    //   },
-    //   async session({ session, user, token }) {
-    //     const extToken = token as ExtendedJWT;
+          token.user = {
+            id: extendedUser.id.toString(),
+            username: extendedUser.username,
+            name: extendedUser.name,
+          };
+        }
+        return token as ExtendedJWT;
+      },
+      async session({ session, user, token }) {
+        const extToken = token as ExtendedJWT;
 
-    //     return {
-    //       ...session,
-    //       user: {
-    //         ...session.user,
-    //         id: extToken.id,
-    //         username: extToken.username,
-    //         name: extToken.name,
-    //       },
-    //     };
-    //   },
-    // },
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            id: extToken.id,
+            username: extToken.username,
+            name: extToken.name,
+          },
+        };
+      },
+    },
     pages: {
       signIn: "/verify-twitter",
     },
