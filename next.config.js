65170c5232a8e86d30327cbd2c81590dccc4b978
Jad Slim <jadslimm@gmail.com>
2023-12-27 17:50:15 -0500

    checkpoint

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-/** @type {import('next').NextConfig} */
-
-const withPWA = require("next-pwa");
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true
+})
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  basePath: "",
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-  },
-});
+  // next.js config
+})
